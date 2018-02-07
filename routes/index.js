8f99160c4070b7814571549bf2c700dc825de3fa
Heitor Dobeis <heitordobeis@gmail.com>
2018-02-07 13:24:36 -0200

    feito

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express'});
 });
+
+/* GET home page. */
+router.get('/new', function(req, res, next) {
+  res.render('new', { title: 'New User' });
+});
+
+
 
 module.exports = router;
